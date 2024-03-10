2351e6f0601c69fcf3501ff05feb67f5c3090957
san-jana <60836837+san-jana@users.noreply.github.com>
2024-03-10 20:56:59 +0530

    Update Library_Management_System.py

--- Library_Management_System.py
+++ Library_Management_System.py
@@ -1,18 +1,34 @@
-from flask import *
+from flask import Flask, request, make_response
+from functools import wraps
 import json, time
 import pyodbc
+from dotenv import load_dotenv
+import os
 
-# Flask app
+# Create a new flask app
 app = Flask(__name__)
 
-# SQL Server connector
+# Establish a SQL Server connection
 myconnection = pyodbc.connect('DRIVER={SQL Server};' 
                       'Server=.\SQLEXPRESS;'
                       'Database=TheLibrary;'
                       'Trusted_Connection=yes')
 cur = myconnection.cursor()
 
-# Basic Authorization
+# Load the login credentials from your environment variables
+load_dotenv()
+lib_username = os.getenv(<'Your Username'>)
+lib_password = os.getenv(<'Your Password'>)
+
+# Authenticate user login using Basic Authorization
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if auth and auth.username == lib_username and auth.password == lib_password:
+            return f(*args, **kwargs)
+        return make_response("Must verify to access The Library!",401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return decorated
 
 
 # Home Page
@@ -22,12 +38,11 @@ def home_page():
     json_dump = json.dumps(data_set)
     return json_dump
 
+
 # Search any book in the Library using a Book ID, Book Name or Author name
 @app.route('/search-book/')
-def search_page():
-    
+def search_page(): 
     try:
-    
         book_query = str(request.args.get('book')) # /search-book/?book=book-id        
         book_queries = [book_query, book_query, book_query]
         books = cur.execute("SELECT * FROM Books \
@@ -36,23 +51,24 @@ def search_page():
                     Authors LIKE ?", book_queries).fetchall()
         
         book_data = {}
-        
         for idx, book in enumerate(books):
-                 book_data[idx] = {'Book ID': book.BookID, 'Book Name': book.BookName, 'Authors': book.Authors,'Book Alias': book.AuthorAlias, 'Book Edition': book.BookEdition, 'Book Dated': book.AddedtoLibDate, 'Book Status': book.BookStatus }
+            book_data[idx] = {'Book ID': book.BookID, 'Book Name': book.BookName, 
+                              'Authors': book.Authors,'Book Alias': book.AuthorAlias, 
+                              'Book Edition': book.BookEdition, 'Book Dated': book.AddedtoLibDate, 
+                              'Book Status': book.BookStatus }
                  
-        cur.commit()
-                        
+        cur.commit()              
         return json.dumps(book_data) if book_data else (f"The Library contains no books with the keyword '{book_query.strip('%')}'.")
-    
-        
+      
     except:
         return "Enter a Book ID, Name or Author to search for a Book."
     
 
-# Get student data using the api endpoint 'http://127.0.0.1:5000/get-student/00062022CHE' where 00062022CHE is a Student ID. 
+# Get any student information using their student ID 
 @app.route('/get-student/<student_id>')
+@auth_required
 def get_student(student_id):
-    get_student_sql = "SELECT * FROM RegisteredStudents WHERE StudentID = ?"
+    get_student_sql = "SELECT * FROM Students WHERE StudentID = ?"
     student = cur.execute(get_student_sql,student_id).fetchone()
     student_data = {"Student ID": student.StudentID,
                     "Student Name": student.FirstName+' '+student.LastName,
@@ -61,22 +77,13 @@ def get_student(student_id):
                     "Number of Books Issued Currently": student.BooksCurrentlyIssued,
                     "Cumulative Number Books Issued": student.TotalBooksIssued,
                     "Is the Student Banned": student.StudentBanned }  
-    cur.commit()
-    
+    cur.commit()    
     return json.dumps(student_data), 200
+    
 
-# add a new book to the database
-'''input must follow the format: 
-{
-    "id": "book id",
-    "name": "book name",
-    "authors": "authors separated by commas"(optional),
-    "alias": common alias the book is referred by (eg. Wren and Martin)(optional),
-    "book edition": edition of the book(optional)} 
-    
-    optional keys must be set equal to null if not used. Eg. "alias" : null '''
-    
+# Add a new book to the database    
 @app.route('/add-book', methods = ['POST'])
+@auth_required
 def add_book():
     book_data = request.get_json()
     book_data = list(book_data.values())
@@ -87,33 +94,90 @@ def add_book():
     return "New book successfully added!"
 
 
-# @app.route('/register-student', methods = ['POST']) -- the library does not need to register students
-# def register_student():
-#     student_data = request.get_json()
-#     return student_data, 201
-
-
 # Issue or re-issue a book
 @app.route('/issue-book', methods = ['POST', 'PATCH'])
+@auth_required
 def issue_book():
-    if request.method == 'POST':
-        issue_data = request.get_json()
-        return issue_data, 201
+    issue_data = request.get_json()
+    student_id , book_id = issue_data['Student ID'], issue_data['Book ID']
     
-    # elif request.method == 'PUT':
-    #     reissue_data = request.get_json()
-    #     return reissue_data, 200
+    if request.method == 'POST':     
+        # get student's information
+        student = cur.execute("SELECT * FROM Students WHERE StudentID = ?", student_id).fetchone()
+        # check if student is banned
+        if student.StudentBanned == 'BANNED':
+            return("Student Banned! Issue Not Allowed.")
+        else:
+            # check if issue limit is exceeded
+            if student.BooksCurrentlyIssued >= 5:
+                return("Issue limit exceeded! Student can only issue 5 books at a time. Issue not allowed.")
+                
+            else:
+                # check book status
+                book = cur.execute("SELECT * FROM Books WHERE BookID = ?", book_id).fetchone()
+                if book.BookStatus == 'Issued':
+                    return("Book issued by another student. Issue a different book.")
+                else:
+                    # Issue Book
+                    cur.execute("INSERT INTO BooksIssued(StudentID, BookID) VALUES (?, ?)", student_id, book_id)
+                    cur.execute("UPDATE Books SET BookStatus = 'Issued' WHERE BookID = ?", book_id)
+                    cur.execute("UPDATE Students SET BooksCurrentlyIssued = BooksCurrentlyIssued + 1, \
+                    TotalBooksIssued = TotalBooksIssued + 1 WHERE StudentID = ?", student_id)
+                    issued_book = cur.execute("SELECT * from BooksIssued WHERE StudentID = ? AND BookID = ?", student_id, book_id).fetchone()
+                    
+                    cur.commit()
+                    return(f"Book issued successfully! Return book by {issued_book.ReturnbyIssueDate}")  
+            
+    # reissue request
+    elif request.method == 'PATCH':       
+        issued_book = cur.execute("SELECT * from BooksIssued WHERE StudentID = ? AND BookID = ?", student_id, book_id).fetchone()
+
+        if not issued_book:
+            return("The student has not issued this book.")
+        else:
+            if issued_book.ReturnStatus == 'Returned':
+                return("Student has returned the book.")
+            elif issued_book.ReturnStatus == 'Issued': 
+                # reissue book
+                cur.execute("UPDATE BooksIssued SET ReissuedOnDate = GETDATE(), ReissueCount = ReissueCount + 1 \
+                WHERE StudentID = ? AND BookID = ?", student_id, book_id)
+                cur.commit()
+                return("Book re-issued")
+        
+    return ("Invalid request!")
     
 
 # Return a book 
 @app.route('/return-book', methods = ['PATCH'])
+@auth_required
 def return_book():
-    book_data =  request.get_json()
-    return book_data, 200
+    return_data =  request.get_json()
+    student_id , book_id = return_data['Student ID'], return_data['Book ID']
+    
+    # Get book details
+    issued_book = cur.execute("SELECT * from BooksIssued WHERE StudentID = ? AND BookID = ?", student_id, book_id).fetchone()
 
-
-# Delete a book
-
+    # Check if book exists in the Issued Books data table
+    if not issued_book:
+        return("The student has not issued this book.") 
+    else:
+        
+        # Check if book has already been returned
+        if issued_book.ReturnStatus == 'Returned':
+            return("Student has returned the book.")
+            
+        elif issued_book.ReturnStatus == 'Issued':
+            # Return book
+            # Update Return Status and Return Date
+            cur.execute("UPDATE BooksIssued SET ReturnStatus = 'Returned', ReturnDate = GETDATE() WHERE StudentID = ? AND BookID = ?", student_id, book_id)
+            # Update Book Availibility Status
+            cur.execute("UPDATE Books SET BookStatus = 'Available' WHERE BookID = ?", book_id)
+            # Update 
+            cur.execute("UPDATE Students SET BooksCurrentlyIssued = BooksCurrentlyIssued - 1 WHERE StudentID = ?", student_id)
+            cur.commit()
+            
+            return("Book returned!")
+        
 
 if __name__ == '__main__':
     app.run(debug = True)
